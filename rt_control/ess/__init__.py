1432630c9a8d6166454a37481525780ac97c524c
David M. Raker <david.raker@pnnl.gov>
2024-07-25 12:15:59 -0700

    Added debugging for ESS subcriptions.

--- rt_control/ess/__init__.py
+++ rt_control/ess/__init__.py
@@ -56,9 +56,11 @@ class EnergyStorageSystem:
             if self.power_reading_point:
                 self.controller.vip.pubsub.subscribe('pubsub', self.bess_topic,
                                                 self._ingest_state(key='power', point_name=self.power_reading_point))
+        _log.debug(f'SUBSCRIPTIONS EXISTING FOR ESS: {self.controller.vip.pubsub.list()}')
 
     def _ingest_state(self, key: str, point_name: str) -> Callable:
         def func(_, __, ___, ____, _____, message):
+            _log.debug(f'Getting point: {point_name} from message: {message}')
             if isinstance(message, list):
                 message = message[0]
             value =  message.get(point_name)
