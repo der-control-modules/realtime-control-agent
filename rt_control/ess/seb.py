869af1a3132dddb59a39caa435bab9911b92a834
David M. Raker <david.raker@pnnl.gov>
2024-07-25 13:19:38 -0700

    Added debugging to SebBESS.

--- rt_control/ess/seb.py
+++ rt_control/ess/seb.py
@@ -1,4 +1,15 @@
+import logging
+
+from importlib.metadata import version
+if int(version('volttron').split('.')[0]) >= 10:
+    from volttron.utils import setup_logging
+else:
+    from volttron.platform.agent.utils import setup_logging
+
 from rt_control.ess import EnergyStorageSystem
+
+setup_logging()
+_log = logging.getLogger(__name__)
 
 
 class SebBESS(EnergyStorageSystem):
@@ -10,4 +21,6 @@ class SebBESS(EnergyStorageSystem):
     @EnergyStorageSystem.power.setter
     def power(self, value: float):
         watts_value = value * 1000
+        _log.debug(f'IN SEB POWER SETTER, with ACTUATOR_VIP: {self.actuator_vip},'
+                   f' ACTUATION_METHOD: {self.actuation_method}, watts_value: {watts_value}')
         self.controller.vip.rpc.call(self.actuator_vip, self.actuation_method, watts_value, point='real')
