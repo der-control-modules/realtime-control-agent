a97901d5143537a257766d95c56688a7659d9cbf
David M. Raker <david.raker@pnnl.gov>
2024-08-30 12:56:28 -0700

    Updated power command in fake ess to match changes in base class.

--- rt_control/ess/fake.py
+++ rt_control/ess/fake.py
@@ -10,7 +10,7 @@ setup_logging()
 _log = logging.getLogger(__name__)
 
 class FakeESS(EnergyStorageSystem):
-    @power.setter
-    def power(self, value: float):
+    @EnergyStorageSystem.power_command.setter
+    def power_command(self, value: float):
         _log.debug(f'Actuating command of: {value}')
         self.states.power = value  # Sets the value without
