c0ff7cc6d152962978368d5d80d05d10d28ed4a6
David M. Raker <david.raker@pnnl.gov>
2025-01-15 10:06:19 -0800

    Correction to function to ingest schedules.

--- rt_control/agent.py
+++ rt_control/agent.py
@@ -61,8 +61,8 @@ class RTControlAgent(Agent):
     def ingest_schedule(self, _, sender, __,  topic, headers, message):
         for dt_string, period in message.items():
             dt = parse_timestamp_string(dt_string)
-            set_point = message.get('bess_setpoints')
-            duration = timedelta(seconds=message.get('duration_in_seconds', 3600))
+            set_point = period.get('bess_setpoints')
+            duration = timedelta(seconds=period.get('duration_in_seconds', 3600))
             if not (dt and set_point):
                 _log.warning(f'Unable to ingest published schedule on {topic} from {sender}'
                              f' for forecast_time: {dt}, message: {message}')
